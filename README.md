48b021903c9cf1f662c49a7192adc7cd5054ea67
David Hirtle <david.hirtle@pulseenergy.com>
2012-01-09 23:36:12 -0800

    Sharing the rmxp love

---
+++ README.md
@@ -0,0 +1,47 @@
+Maplinks (v 1.0)
+===
+
+A script for [RPG Maker XP](http://en.wikipedia.org/wiki/RPG_Maker_XP), which uses Ruby. Written years ago and posted on some now-defunct forums, but may still be useful to folks.
+
+A common question among RMXP beginners is "how do you connect maps?" or "why are so many teleports needed?". The typical response is one of the following:
+
+* Block off most spaces with terrain and use a few teleport events
+* Make the map larger
+* Do some event scripting (using a parallel process event to set variables[/list]
+
+These solutions aren't optimal -- causing lag and/or inconvenience for the mapper.
+
+This script is designed to help. Maplinks simplify linking maps together: a single event sets up an entire edge of the map as a teleport to another map. Players trying to leave that edge of the current map are automatically teleported.
+
+All it takes is one event per edge of the map:
+
+![Maplinks screenshot](http://s88387243.onlinehome.us/rmxp/maplinks/maplinks.jpg)
+
+Features
+---
+
+* easy to setup and use
+* save time, and possibly reduce lag
+* handles move events (jumping, speed changes, etc)
+* doesn't interfere with "normal" teleports
+
+
+Demo
+---
+See `demo` directory. Requires RMXP, of course.
+
+
+Installation
+---
+Copy the script in `src`, and open the Script Editor within RMXP. At the bottom of the list of classes on the left side of the new window, you'll see one called "Main". Right click on it and select "Insert". In the newly created entry in the list, paste this script.
+
+Note that this script uses the [SDK](http://www.hbgames.org/forums/viewtopic.php?t=1802.0), which must be above this script. When it was developed, SDK 1.5 was the latest version.
+
+To avoid this dependency, remove or comment out the 3 SDK-related lines, including the `end` at the bottom.
+
+Usage
+---
+
+To link a map with another on a specific edge (north, east, south or west), create an event with <maplink> included in its name on the appropriate edge of the map. (To avoid confusion, maplink events on corners of the map are not valid.) Then, add a teleport ("Transfer Player") command to the event to specify the destination map and other details (e.g. player direction,  fading on/off). If the destination is an east or west edge, then the Y  coordinate is calculated based on the player's Y coordinate when  teleporting; likewise, the X coordinate is calculated automatically when the destination is a north or south edge.
+ 
+Note: unlike normal teleport events, maplinks are activated when the player tries to leave the screen instead of when stepping on the last tile. This  behaviour could be changed, but I feel that it's more natural this way: it leaves the whole map open for actual exploration, instead of "wasting" the outer tiles of a map.
---
+++ src/maplinks.rb
@@ -0,0 +1,141 @@
+#==============================================================================
+# ** Maplinks
+#------------------------------------------------------------------------------
+# Wachunga
+# Version 1.0
+# 2006-10-29
+# See https://github.com/Wachunga/rmxp-maplinks for details
+#------------------------------------------------------------------------------
+
+#------------------------------------------------------------------------------
+# * SDK Log Script
+#------------------------------------------------------------------------------
+SDK.log('Maplinks', 'Wachunga', 1.0, '2006-10-29')
+
+#------------------------------------------------------------------------------
+# * Begin SDK Enabled Check
+#------------------------------------------------------------------------------
+if SDK.state('Maplinks') == true
+
+
+class Game_Event < Game_Character
+  
+  alias wachunga_ml_game_event_init initialize
+  def initialize(map_id, event)
+    wachunga_ml_game_event_init(map_id, event)
+    # check if event includes a maplink
+    if @event.name.upcase.include?('<MAPLINK>')
+      dir = nil
+      if @event.y == $game_map.height-1
+        dir = 2 unless @event.x == 0 or @event.x == $game_map.width-1
+      elsif @event.x == 0
+        dir = 4 unless @event.y == 0 or @event.y == $game_map.height-1
+      elsif @event.x == $game_map.width-1
+        dir = 6 unless @event.y == 0 or @event.y == $game_map.height-1
+      elsif @event.y == 0
+        dir = 8 unless @event.x == 0 or @event.x == $game_map.width-1
+      end
+      if dir != nil
+        @list.each { |command|
+          if command.code == 201
+            # make sure new location isn't specified by variables
+            if command.parameters[0] == 0
+              $game_map.add_maplink(dir, command.parameters)
+              break
+            end
+          end
+        }
+      end
+    end
+  end
+  
+end
+
+#------------------------------------------------------------------------------
+
+class Game_Map
+  
+  alias wachunga_ml_gm_setup setup
+  def setup(map_id)
+    @maplinks = {}
+    wachunga_ml_gm_setup(map_id)
+  end
+  
+  #----------------------------------------------------------------------------
+  # * Maplink Addition
+  #     dir        : direction of maplink to be added
+  #     parameters : teleportation parameters (destination map id, x, y, etc.)
+  #----------------------------------------------------------------------------
+  def add_maplink(dir, parameters)
+    @maplinks[dir] = parameters
+  end
+
+  #----------------------------------------------------------------------------
+  # * Maplink Activation
+  #     dir : direction of maplink to be activated
+  #----------------------------------------------------------------------------
+  def activate_maplink(dir)
+    return if @maplinks[dir] == nil
+    params = @maplinks[dir]
+    # params[1] = map id
+    # params[2] = x
+    # params[3] = y
+    width = load_data(sprintf("Data/Map%03d.rxdata", params[1])).width
+    height = load_data(sprintf("Data/Map%03d.rxdata", params[1])).height
+    # copy current x coordinate if north/south or y coordinate if east/west
+    if (dir == 2 and params[3] == 0) or (dir == 8 and params[3] == height-1)
+      params[2] = $game_player.x
+    elsif (dir == 4 and params[2] == width-1) or (dir == 6 and params[2] == 0)
+      params[3] = $game_player.y
+    end
+    # set up a dummy interpreter just for teleport
+    interpreter = Interpreter.new
+    interpreter.parameters = params
+    interpreter.index = 0
+    interpreter.command_201
+  end
+
+end
+
+#------------------------------------------------------------------------------
+
+class Interpreter
+  attr_accessor :parameters
+  attr_accessor :index
+end
+
+#------------------------------------------------------------------------------
+
+class Game_Player
+
+  alias wachunga_ml_game_player_cett check_event_trigger_touch
+  def check_event_trigger_touch(x, y)
+    check_maplinks(x,y)
+    wachunga_ml_game_player_cett(x,y)
+  end
+  
+  #----------------------------------------------------------------------------
+  # * Maplink Checking
+  #     x : x coordinate of map to check for a maplink
+  #     y : y coordinate of map to check for a maplink
+  #----------------------------------------------------------------------------
+  def check_maplinks(x,y)
+    if $game_map.valid?(x,y) then return end
+    dir = nil
+    if y == $game_map.height then dir = 2
+    elsif x == -1 then dir = 4
+    elsif x == $game_map.width then dir = 6
+    elsif y == -1 then dir = 8
+    end
+    if dir != nil
+      $game_map.activate_maplink(dir)
+    end
+  end
+
+end
+
+#------------------------------------------------------------------------------
+# * End SDK Enable Test
+#------------------------------------------------------------------------------
+end
+
